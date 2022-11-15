758b9b6722b1a20d75262f8ad4c3a962cd21a8d2
claudiacastella <claudiacastella1984@gmail.com>
2022-11-15 20:19:41 +0100

    Added Burguer menu without js

---
+++ index.js
@@ -0,0 +1,11 @@
+// Adding burguer menu
+/*
+function myFunction() {
+    var x = document.getElementById("nav-bar");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  */
