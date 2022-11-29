250af5a2321f4aa8f3c11b2141a2d3dac1460dc3
claudiacastella <claudiacastella1984@gmail.com>
2022-11-29 16:16:27 +0100

    Fix contact css & JS first iteration

--- index.js
+++ index.js
@@ -6,3 +6,43 @@ burguerIcon.addEventListener('click', () => {
   burguer.classList.toggle("hidden")
 } )
 
+// API
+async function callApi() {
+  const res = await fetch('https://mockyard.herokuapp.com/products');
+  const finalRes = await res.json(); 
+  
+  let i = Math.floor(Math.random() * (60 - 0 + 1) + 0);
+  document.querySelector(".article-title").innerText = finalRes[i].name;
+  document.querySelector(".article-summary").innerText = finalRes[i].description;
+  document.querySelector(".timestamp").innerText = finalRes[i].createdAt;
+  document.querySelector(".timestamp").setAttribute("dateTime", `${finalRes[i].createdAt}`);
+}
+
+async function callImg() {
+  const imgCall = await fetch('https://dog.ceo/api/breeds/image/random');
+  const finalImg = await imgCall.json();
+
+  const image = finalImg.message;
+  document.querySelector(".article-img").setAttribute("src", `${image}`);
+}
+
+const articles = document.querySelectorAll(".artic");
+
+articles.forEach (e => {
+  callApi(e);
+  callImg(e);}
+)
+
+// Averiguar cómo hacer que se repita en las 3 cards, y no que me printe solo la primera. 
+
+
+
+// FORM
+
+const submit = document.querySelector("#submit-btn");
+
+
+submit.addEventListener("click",() =>{
+
+
+})
